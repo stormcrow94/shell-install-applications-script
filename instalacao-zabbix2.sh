2504ae0f007448d7acc54317147cae3500fd6c23
Luciano D. Matos <112457329+stormcrow94@users.noreply.github.com>
2024-05-15 03:27:31 -0300

    Update instalacao-zabbix2.sh

--- instalacao-zabbix2.sh
+++ instalacao-zabbix2.sh
@@ -13,11 +13,65 @@ yum install zabbix-agent -y
 HOSTNAME=$(hostname)
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
 
-# Configure zabbix_agentd.conf
-sed -i "s/^Server=.*/Server=$ZABBIX_PROXY_SERVER/" /etc/zabbix/zabbix_agentd.conf
-sed -i "s/^ServerActive=.*/ServerActive=$ZABBIX_PROXY_SERVER/" /etc/zabbix/zabbix_agentd.conf
-sed -i "s/^Hostname=.*/Hostname=$HOSTNAME/" /etc/zabbix/zabbix_agentd.conf
-sed -i "s/# StartAgents=.*/StartAgents=3/" /etc/zabbix/zabbix_agentd.conf
+# Create a new zabbix_agentd.conf configuration file
+cat <<EOL > /etc/zabbix/zabbix_agentd.conf
+# This is a configuration file for Zabbix agent daemon (Unix)
+# To get more information about Zabbix, visit http://www.zabbix.com
+
+############ GENERAL PARAMETERS #################
+
+### Option: PidFile
+# Name of PID file.
+PidFile=/var/run/zabbix/zabbix_agentd.pid
+
+### Option: LogFile
+# Log file name for LogType 'file' parameter.
+LogFile=/var/log/zabbix/zabbix_agentd.log
+
+### Option: LogFileSize
+# Maximum size of log file in MB.
+LogFileSize=10
+
+### Option: DebugLevel
+# Specifies debug level:
+# 0 - basic information about starting and stopping of Zabbix processes
+# 1 - critical information
+# 2 - error information
+# 3 - warnings
+# 4 - for debugging (produces lots of information)
+# 5 - extended debugging (produces even more information)
+DebugLevel=3
+
+### Option: Server
+# List of comma delimited IP addresses of Zabbix servers and Zabbix proxies.
+Server=$ZABBIX_PROXY_SERVER
+
+### Option: ListenPort
+# Agent will listen on this port for connections from the server.
+ListenPort=10050
+
+### Option: ListenIP
+# List of comma delimited IP addresses that the agent should listen on.
+ListenIP=0.0.0.0
+
+### Option: StartAgents
+# Number of pre-forked instances of zabbix_agentd that process passive checks.
+StartAgents=3
+
+### Option: ServerActive
+# List of comma delimited IP:port pairs of Zabbix servers and Zabbix proxies for active checks.
+ServerActive=$ZABBIX_PROXY_SERVER:10051
+
+### Option: Hostname
+# Unique, case sensitive hostname.
+Hostname=$HOSTNAME
+
+### Option: Include
+# You may include individual files or all files in a directory in the configuration file.
+Include=/etc/zabbix/zabbix_agentd.d/*.conf
+
+############ END OF CONFIGURATION #################
+EOL
 
 # Start and enable Zabbix agent
 systemctl restart zabbix-agent
